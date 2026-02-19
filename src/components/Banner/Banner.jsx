4f1e9c9e8cbeb835f5f9c6078c722be4890a6df4
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-02-19 16:30:43 +0600

    banner border added

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className=" bg-[#EFEFEF] pt-6">
+      <div className=" max-w-11/12 mx-auto min-h-screen border-3 border-white rounded-3xl"></div>
+    </div>
+  );
+};
+
+export default Banner;
